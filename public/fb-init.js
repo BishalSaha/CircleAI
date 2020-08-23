9f9b3862fcd0739d0f8b46ad698fb6e6285bc5e0
Prasanna Neelavar <prasanna.neelavar@gmail.com>
2020-08-23 19:10:28 +0530

    fix: firebase errors, separate contact & subscribe

---
+++ public/fb-init.js
@@ -0,0 +1,17 @@
+// import 'firebase/app';
+// import * as fbAnalytics from 'firebase/analytics';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyCBhLlik0YD-nP9IkdMmxNeUnXzAv3A-Cs',
+  authDomain: 'boring-men.firebaseapp.com',
+  databaseURL: 'https://boring-men.firebaseio.com',
+  projectId: 'boring-men',
+  storageBucket: 'boring-men.appspot.com',
+  messagingSenderId: '956670404039',
+  appId: '1:956670404039:web:a5c928c9707af6d38965e2',
+  measurementId: 'G-1LT5NFKT0B',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// fbAnalytics.analytics();
